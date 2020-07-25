24a8b9a1b9334ccf9b19f145f058bb84d2310f07
Ali Abbas Rizvi <aliabbasrizvi777@gmail.com>
2020-07-25 16:37:42 +0530

    Created user authentication

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 var express = require("express"),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose"),
-  Teacher = require("./models/Teacher");
+  Teacher = require("./models/Teacher"),
+  Student = require("./models/Student"),
+  Admin = require("./models/Admin"),
+  passport = require("passport"),
+  localStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose");
 
 var app = express();
 app.listen(3000, () => {
@@ -26,6 +31,20 @@ mongoose
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use("student", new localStrategy(Student.authenticate()));
+passport.use("teacher", new localStrategy(Teacher.authenticate()));
+passport.use("admin", new localStrategy(Admin.authenticate()));
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  if (user != null) {
+    done(null, user);
+  }
+});
 // app.use(flash());
 
 app.use(
@@ -35,3 +54,99 @@ app.use(
     saveUninitialized: false,
   })
 );
+
+isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+app.post("/register", (req, res) => {
+  if (req.body.category == "Teacher") {
+    var newUser = new Teacher({
+      username: req.body.username,
+      slot: req.body.slot,
+    });
+    Teacher.register(newUser, req.body.password, async function (err, user) {
+      if (err) {
+        console.log(err);
+        // req.flash("error1", "Username already exists");
+        res.redirect("/login");
+      } else {
+        await Teacher.authenticate("local")(req, res, function () {
+          res.redirect("/dashboard");
+          res.send("Registered");
+        });
+      }
+    });
+  } else if (req.body.category == "Student") {
+    var newUser = new Student({
+      username: req.body.username,
+      // email: req.body.email,
+      // dob: req.body.dob,
+      // qualification: req.body.qualification,
+    });
+    Student.register(newUser, req.body.password, async function (err, user) {
+      if (err) {
+        console.log(err);
+        // req.flash("error1", "Username already exists");
+        res.redirect("/login");
+      } else {
+        await Student.authenticate("local")(req, res, function () {
+          res.redirect("/dashboard");
+          res.send("Registered");
+        });
+      }
+    });
+  } else {
+    var newUser = new Admin({
+      username: req.body.username,
+      email: req.body.email,
+    });
+    Admin.register(newUser, req.body.password, async function (err, user) {
+      if (err) {
+        console.log(err);
+        // req.flash("error1", "Username already exists");
+        res.redirect("/login");
+      } else {
+        await Admin.authenticate("local")(req, res, function () {
+          res.redirect("/dashboard");
+          res.send("Registered");
+        });
+      }
+    });
+  }
+});
+
+app.post(
+  "/login-student",
+  passport.authenticate("student", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+  }),
+  (req, res) => {}
+);
+
+app.post(
+  "/login-teacher",
+  passport.authenticate("teacher", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+  }),
+  (req, res) => {}
+);
+
+app.post(
+  "/login-admin",
+  passport.authenticate("admin", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+  }),
+  (req, res) => {}
+);
+
+app.get("/dashboard", (req, res) => {
+  res.render("home");
+});
