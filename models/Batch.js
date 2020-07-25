696937564084582b9786cce8d15640c0324062c5
Ali Abbas Rizvi <aliabbasrizvi777@gmail.com>
2020-07-26 04:52:50 +0530

    spelling

---
+++ models/Batch.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BatchSchema = new Schema({
+  Teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Teacher",
+    default: null,
+  },
+  Students: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+      default: null,
+    },
+  ],
+  Time: {
+    type: Number,
+    default: null,
+  },
+  level: {
+    type: String,
+  },
+});
+const Batch = mongoose.model("Batch", BatchSchema);
+
+module.exports = module.exports = Batch;
